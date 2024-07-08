f9bcac8294f5b23ddba5f1d8d57755604ae3c38f
Andy Tuan <abc@gmail.com>
2024-07-08 15:31:40 +0700

    Chapter 11 Account Activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,40 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params.dig(:session, :email)&.downcase
-    if user&.authenticate params.dig(:session, :password)
-      log_in user
-      params.dig(:session, :remember_me) == "1" ? remember(user) : forget(user)
-      redirect_to user, status: :see_other
+    if user&.authenticate(params.dig(:session, :password))
+      if user.activated?
+        handle_session_management(user)
+      else
+        handle_inactive_user
+      end
     else
-      flash.now[:danger] = t "sessions.invalid_email_password_combination"
-      render :new, status: :unprocessable_entity
+      handle_invalid_credentials
     end
   end
 
   def destroy
     log_out
     redirect_to root_url, status: :see_other
+  end
+
+  private
+
+  def handle_session_management user
+    forwarding_url = session[:forwarding_url]
+    reset_session
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+    log_in user
+    redirect_to forwarding_url || user
+  end
+
+  def handle_inactive_user
+    flash[:warning] = "Account not activated. " \
+                  "Check your email for the activation link."
+    redirect_to root_url, status: :see_other
+  end
+
+  def handle_invalid_credentials
+    flash.now[:danger] = t("invalid_email_password_combination")
+    render :new, status: :unprocessable_entity
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,28 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  host = "localhost:3000"
+  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: host}
+  
+  # SMTP settings for gmail
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   user_name: ENV["USER_EMAIL"],
+  #   password: ENV["USER_PASSWORD"],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true
+  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '7c6eadaaefb33e',
+    :password => '50abeabd12aff8',
+    :address => 'sandbox.smtp.mailtrap.io',
+    :host => 'sandbox.smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :login
+  }
 end

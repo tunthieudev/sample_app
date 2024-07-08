47e8777217b9eb9ae7ae9cc4857d3008cddcc19d
Andy Tuan <abc@gmail.com>
2024-07-08 15:31:40 +0700

    Chapter 12: Reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  resources :products
   get "demo_partials/new"
   get "demo_partials/edit"
-  root "static_pages#home"
   get "static_pages/home"
   get "static_pages/help"
-
+  
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users
+  resources :password_resets, only: %i(new create edit update)
   resources :account_activations, only: :edit
+  resources :products
 end
